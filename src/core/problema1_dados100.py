df1bed9ab8917baeec6f503d233280f09abb4a54
tonussi <lptonussi@gmail.com>
2017-04-30 22:43:34 -0300

    Add titles

--- src/core/problema1_dados100.py
+++ src/core/problema1_dados100.py
@@ -6,6 +6,7 @@ def problema1_normal_100_dados():
     s = np.random.normal(mu, sigma, 100)
     count, bins, ignored = plt.hist(s, 30, normed=True)
     plt.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
+    plt.title('problema1_normal_100_dados')
     plt.show()
 
 def problema1_expo_100_dados():
@@ -13,6 +14,7 @@ def problema1_expo_100_dados():
     s = np.random.exponential(mu, 100)
     count, bins, ignored = plt.hist(s, 30, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
+    plt.title('problema1_expo_100_dados')
     plt.show()
 
 def problema1_triangular_100_dados():
@@ -20,6 +22,7 @@ def problema1_triangular_100_dados():
     s = np.random.triangular(mu, mode, sigma, 100)
     count, bins, ignored = plt.hist(s, 30, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
+    plt.title('problema1_triangular_100_dados')
     plt.show()
 
 def problema1_uniforme_100_dados():
@@ -27,6 +30,7 @@ def problema1_uniforme_100_dados():
     s = np.random.uniform(mu, sigma, 100)
     count, bins, ignored = plt.hist(s, 30, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
+    plt.title('problema1_uniforme_100_dados')
     plt.show()
 
 problema1_normal_100_dados()
