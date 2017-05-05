d0eadefd2abeb22e9a59cbc02fffed09149b3ebf
tonussi <lptonussi@gmail.com>
2017-05-04 21:17:43 -0300

    Adjust bins

--- src/core/problema1_dados25.py
+++ src/core/problema1_dados25.py
@@ -4,7 +4,7 @@ import numpy as np
 def problema1_normal_25_dados():
     mu, sigma = 15, 3
     s = np.random.normal(mu, sigma, 25)
-    count, bins, ignored = plt.hist(s, 30, normed=True)
+    count, bins, ignored = plt.hist(s, 5, normed=True)
     plt.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
     plt.title('problema1_normal_25_dados')
     plt.show()
@@ -12,7 +12,7 @@ def problema1_normal_25_dados():
 def problema1_expo_25_dados():
     mu, sigma = 5, 5
     s = np.random.exponential(mu, 25)
-    count, bins, ignored = plt.hist(s, 30, normed=True)
+    count, bins, ignored = plt.hist(s, 5, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
     plt.title('problema1_expo_25_dados')
     plt.show()
@@ -20,7 +20,7 @@ def problema1_expo_25_dados():
 def problema1_triangular_25_dados():
     mu, mode, sigma = 1, 5, 7
     s = np.random.triangular(mu, mode, sigma, 25)
-    count, bins, ignored = plt.hist(s, 30, normed=True)
+    count, bins, ignored = plt.hist(s, 5, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
     plt.title('problema1_triangular_25_dados')
     plt.show()
@@ -28,7 +28,7 @@ def problema1_triangular_25_dados():
 def problema1_uniforme_25_dados():
     mu, sigma = 10, 20
     s = np.random.uniform(mu, sigma, 25)
-    count, bins, ignored = plt.hist(s, 30, normed=True)
+    count, bins, ignored = plt.hist(s, 5, normed=True)
     plt.plot(bins, 1 / (mu * np.sqrt(2 * np.pi)) * np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)), linewidth = 2, color = 'r')
     plt.title('problema1_uniforme_25_dados')
     plt.show()
